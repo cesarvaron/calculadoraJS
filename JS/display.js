f6ecf53e48b2b7ffa5d1e82998d1ff2ef9cfda9b
cesarvaron <112278536+cesarvaron@users.noreply.github.com>
2022-09-18 08:40:07 -0500

    Commit funcionando
    revisar textos faltantes para acomodar

--- JS/display.js
+++ JS/display.js
@@ -1,4 +1,63 @@
 //constructor
+class Display{
+    constructor(displayValorAnterior,displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();//OBJETO TIPO CALCULADORA LE DA PROPIEDFADES AL CONTRUCTOR SE LLAMA LOS OPERADORES
+        this.tipoOperacion = undefined;
+        this.valorActual= '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            restar: '-',
+            multiplicar: 'X',
+            dividir: '%',
+
+        }
+    }
+
+
+    borrar() {
+        this.ValorActual = this.ValorActual.toString().slice(0, -1); //metodo tosString convierte a cadena caracteres y slice se encarga de borrar la cadena que este en la variable
+        this.imprimirValores();
+    }
+
+    borrarTodo() {
+        this.ValorActual = ''; //ingresa valor vacio
+        this.ValorAnterior = ''; 
+        this.tipoOperacion = undefined; //limpoia el borrado sin definicion
+        this.imprimirValores(); 
+
+    }
+
+    computar(tipo) { //valida qye se realice la operacion
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo; //llega del metodo la palabra computar
+        this.valorAnterior = this.valorActual || this.ValorAnterior; 
+        valorActual = '';
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        if (numero === '.' && this.valorActual.includes('.')) return //ver metodo includes compara valores dentro del array
+        this.valorActual = this.valorActual.toString() + numero.toString(); 
+    }
+
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.ValorAnterior} ${this.signos[this.tipoOrecion] || ''}`;
+    }
+
+    calcular(){
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if(isNaN(valorActual) || isNaN(valorAnterior)) return 
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+
+    }
+}
+
 
 
 //calculos
--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,13 @@
 const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
-const botonesNumeros = document.getElementById('.numero');
-const botonesOperadores = document.getElementById('.operador');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
 
 const display = new Display(displayValorAnterior, displayValorActual);
 
-botonesNumeros.forEach(boton => {
-    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML)); 
-    
-});
-botonesOperadores.forEach(boton => {
-    boton.addEventListener('click', () => display.computar(boton.value));
-}); 
+botonesNumeros.forEach(boton => { boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML)); });
+
+botonesOperadores.forEach(boton => { boton.addEventListener('click', () => display.computar(boton.value)); }); 
 
 
 
